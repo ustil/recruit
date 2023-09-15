1def1be5157243c47d2fb4b9c2178035e4aadf97
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2023-09-15 15:44:44 +0800

    Initialize the repository

---
+++ api/index.ts
@@ -0,0 +1,50 @@
+import { handle } from '@hono/node-server/vercel'
+import { PrismaClient } from '@prisma/client'
+import { Hono } from 'hono'
+
+interface Student {
+  id: string
+  name: string
+  school: string
+  major: string
+  score?: number
+  isPassed?: boolean
+  note?: string
+}
+
+const prisma = new PrismaClient()
+const app = new Hono().basePath('/api')
+
+app.post('/student', async (c) => {
+  const student = await c.req.json()
+  await prisma.student.create({ data: student })
+  return c.text('', 201)
+})
+
+app.get('/student/:id', async (c) => {
+  const { id } = c.req.param()
+  const student = await prisma.student.findUnique({
+    where: { id },
+    select: { id: true, name: true, isPassed: true },
+  })
+  return student !== null ? c.json(student) : c.text('', 404)
+})
+
+app.get('/students', async (c) => {
+  const students = await prisma.student.findMany()
+  return c.json(students)
+})
+
+app.put('/students', async (c) => {
+  const students = (await c.req.json()) as Student[]
+  const execution = students.map((student) =>
+    prisma.student.update({
+      where: { id: student.id },
+      data: student,
+    })
+  )
+  await Promise.all(execution)
+  return c.text('', 200)
+})
+
+export default handle(app)
